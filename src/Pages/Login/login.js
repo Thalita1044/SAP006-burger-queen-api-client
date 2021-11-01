a75d56384ad4e5fd09e382c0c0dc8822d29886e1
Beatriz Ferraz <beaferraz2010@gmail.com>
2021-11-01 16:30:47 -0300

    login api

--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import Button from "../../Components/Button/button";
 import InputText from "../../Components/input/input";
 import "./login.css";
-import Logo from '../../img/logo-img-login';
 
 
 const Login = () => {
@@ -21,7 +20,18 @@ const Login = () => {
     if (userPassword.length < 6) {
       setVerifyPassword(true);
     } else {
-      console.log("entrou no else");
+      fetch("https://lab-api-bq.herokuapp.com/auth", {
+        method: "POST", //criar uma nova entrada de usuario
+        headers: {
+          // eslint-disable-next-line quote-props
+         "accept" : "application/json",
+
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `email=${userEmail}&password=${userPassword}`,
+        
+      })
+      //console.log("entrou no else");
     }
 
     //console.log("cadastrar user")
@@ -29,44 +39,47 @@ const Login = () => {
 
   return (
 
-    <div className="login-container">
-         <img className="logo-img-login" src={Logo} alt="Logo naruto" ></img>
-        <form className="login-form">
-          <h1 className="text-login">Login</h1>
-          <InputText
-            inputType="text"
-            inputPlaceholder=" Digite seu Email"
-            inputValue={userEmail}
-            inputOnChange={(event) => setUserEmail(event.target.value)}
-          />
+    <section className="container">
+        <div className="login-container">
+          <form className="container-form">
+            <h1 className="text-form">Login</h1>
+            
 
-          <InputText
-            InputType="password"
-            inputPlaceholder=" Senha"
-            inputValue={userPassword}
-            inputOnChange={(event) => setUserPassword(event.target.value)}
-          />
+            <InputText
+              inputType="text"
+              inputPlaceholder=" Digite seu Email"
+              inputValue={userEmail}
+              inputOnChange={(event) => setUserEmail(event.target.value)}
+            />
 
-          <Button
-            buttonOnClick={(e) => passwordlogin(e)}
-            buttonClass="btn-form"
-          >
-            entrar
-          </Button>
-          {verifyPassword ? (
-            <div className="erro">
-              <h2>preencha o campo corretamente</h2>
-            </div>
-          ) : null}
-          {verifyInput ? (
-            <div className="erro">
-              <h2>preencha o campo corretamente</h2>
-            </div>
-          ) : null}
-          
-        </form>
-      <Link to="/Cadastro">Cadastre aqui</Link>
-   </div>
+            <InputText
+              InputType="password"
+              inputPlaceholder=" Senha"
+              inputValue={userPassword}
+              inputOnChange={(event) => setUserPassword(event.target.value)}
+            />
+
+            <Button
+              buttonOnClick={(e) => passwordlogin(e)}
+              buttonClass="btn-form"
+            >
+              entrar
+            </Button>
+            {verifyPassword ? (
+              <div className="erro">
+                <h2>preencha o campo corretamente</h2>
+              </div>
+            ) : null}
+            {verifyInput ? (
+              <div className="erro">
+                <h2>preencha o campo corretamente</h2>
+              </div>
+            ) : null}
+            
+          </form>
+          <Link to="/Cadastro">Cadastre aqui</Link>
+        </div>
+    </section>
   );
 };
 export default Login;
